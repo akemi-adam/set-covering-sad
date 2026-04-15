13b022f64b8c965275bd8025e29893829627ad27
Mosiah Adam <tech.adam.servicos@gmail.com>
2026-04-15 16:09:53 -0300

    feat: adiciona busca tabu

--- tabu_search.py
+++ tabu_search.py
@@ -52,3 +52,25 @@ class SolveSCP:
             neighbors.append((new_solution, move))
         return neighbors
 
+    def exec(self) -> set:
+        '''
+        Algoritmo do Tabu Search. Percorre um número X de interações, pegando os vizinhos e ordenando eles com base no seu custo.
+        
+        Depois, percorre cada vizinho. Se o movimento não estiver na lista tabu ou seu custo for menor que o da melhor solução atual, promove ele a candidato atual e proibe o movimento.
+        '''
+        current: set = self.initial_solution()
+        best: set = current.copy()
+        tabu_list: list = []
+        for _ in range(self.max_iter):
+            neighbors = self.get_neighbors(current)
+            neighbors.sort(key=lambda neighbor: self.cost(neighbor[0]))
+            for candidate, move in neighbors:
+                if move not in tabu_list or self.cost(candidate) < self.cost(best):
+                    current = candidate
+                    tabu_list.append(move)
+                    if len(tabu_list) > self.max_movs_size:
+                        tabu_list.pop(0)
+                    break
+            if self.cost(current) < self.cost(best):
+                best = current.copy()
+        return best
